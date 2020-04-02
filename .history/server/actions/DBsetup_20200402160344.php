ef60092c29f25574bb297d8933faba874d4622ba
Ruba abu isa <rubaaboissa@gmail.com>
2020-04-02 16:28:23 +0200

    small change in read.php

---
+++ .history/server/actions/DBsetup_20200402160344.php
@@ -0,0 +1,14 @@
+<?php
+ // Database connectopn
+    $DBhost = '173.212.235.205';
+    $DBuser = 'rubacode_rubacodefactory';
+    $DBpassword = 'Xer{QLc2!2LT';
+    $DBname = 'rubacode_api';   
+    
+    $conn= mysqli_connect($DBhost, $DBuser, $DBpassword, $DBname);
+
+            if ($conn->connect_error) {
+                $conn= null;
+            }
+            
+
